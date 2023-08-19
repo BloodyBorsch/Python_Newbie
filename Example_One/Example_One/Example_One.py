89d93e3d88b565a26980f7ce9f64fc26288762bc
Maks <bismard@rambler.ru>
2023-08-19 22:45:57 +0300

    First_Start

---
+++ Example_One/Example_One/Example_One.py
@@ -0,0 +1,19 @@
+class LessonOne:
+
+    count = 0
+    distance = 0
+    firstFriendSpeed = 1
+    secondFriendSpeed = 2
+    dogSpeed = 5
+    minDistanceBetween = 10
+    
+    #boolDiscription = DistanceCheck(distance, minDistanceBetween)
+    #print()
+
+    def TimeCount(distance, speedOfFriend, speedOfDog):        
+        distance/(speedOfFriend + speedOfDog)
+        
+    def DistanceCheck(distanceBetween, minDistance):       
+        return(bool(distanceBetween > minDistance))
+    
+    
