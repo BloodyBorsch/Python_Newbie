c699f6104455c7e0b9d0750c07bac0fb8fdaa67e
Maks <bismard@rambler.ru>
2023-08-23 21:34:04 +0300

    LessonThree
    Massive added

--- Example_One/Example_One/Example_One.py
+++ Example_One/Example_One/Example_One.py
@@ -1,31 +1,41 @@
-class LessonOne:
+# class LessonOne:
 
-    count = 0    
-    firstFriendSpeed = 1
-    secondFriendSpeed = 2
-    dogSpeed = 5
-    distance = 0
-    minDistanceBetween = 10
+#     count = 0    
+#     firstFriendSpeed = 1
+#     secondFriendSpeed = 2
+#     dogSpeed = 5
+#     distance = 0
+#     minDistanceBetween = 10
     
-    def TimeCount(distance, speedOfFriend, speedOfDog):        
-        distance/(speedOfFriend + speedOfDog)
+#     def TimeCount(distance, speedOfFriend, speedOfDog):        
+#         distance/(speedOfFriend + speedOfDog)
         
-    def DistanceCheck(distanceBetween, minDistance):       
-        return(bool(distanceBetween > minDistance))    
+#     def DistanceCheck(distanceBetween, minDistance):       
+#         return(bool(distanceBetween > minDistance))    
     
-    #print(DistanceCheck(distance, minDistanceBetween))            
+#     print(DistanceCheck(distance, minDistanceBetween))            
 
-class LessonTwo:
+# class LessonTwo:
     
-    count = 0
-    factorial = int(input())    
-    solution = 1
+#     count = 0
+#     factorial = int(input())    
+#     solution = 1
 
-    while count < factorial:
-        count += 1
-        solution *= count
-    else:
-        print(solution)
+#     while count < factorial:
+#         count += 1
+#         solution *= count
+#     else:
+#         print(solution)
+        
+class LessonThree:
+    
+    massive = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    
+    # for elem in massive:
+    #     if elem < 5:
+    #         print(elem)
+           
+    print([elem for elem in massive if elem < 5])
         
     
     
