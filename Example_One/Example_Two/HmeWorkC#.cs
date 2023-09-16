b3ecc7ac6744c4519acefcec316e77a0657e532e
Maks <bismard@rambler.ru>
2023-09-16 23:00:45 +0300

    C# project

---
+++ Example_One/Example_Two/HmeWorkC#.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeWork
+{
+    public class Example1
+    {
+        private static int _numberOne = 0;
+        private static int _numberTwo = 10;        
+
+        private static void Main(string[] args)
+        {           
+            Randomizer(_numberOne, _numberTwo);
+        }
+
+        private static void Randomizer(int min, int max)
+        {
+            Console.WriteLine(new Random().Next(min, max));            
+        }
+    }
+}
