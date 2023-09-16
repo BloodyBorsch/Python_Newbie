e49dd101223c84f76cab9eaaed01c5ec668afd4e
Maks <bismard@rambler.ru>
2023-09-16 23:55:07 +0300

    Training

--- Example_One/Example_Two/HmeWorkC#.cs
+++ Example_One/Example_Two/HmeWorkC#.cs
@@ -1,18 +1,55 @@
-﻿namespace HomeWork
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+
+int xa = 40;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 80;
+int yc = 30;
+
+int x = 0;
+int y = 0;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+for (int count = 0; count < 10000; count++)
 {
-    public class Example1
+    int what = new Random().Next(0, 3);
+
+    Switcher(what);
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+
+    void Switcher(int number)
     {
-        private static int _numberOne = 0;
-        private static int _numberTwo = 10;        
-
-        private static void Main(string[] args)
-        {           
-            Randomizer(_numberOne, _numberTwo);
-        }
-
-        private static void Randomizer(int min, int max)
+        switch (number)
         {
-            Console.WriteLine(new Random().Next(min, max));            
+            case 0:
+                x = (x + xa) / 2;
+                y = (y + ya) / 2;
+                break;
+            case 1:
+                x = (x + xb) / 2;
+                y = (y + yb) / 2;
+                break;
+            case 2:
+                x = (x + xc) / 2;
+                y = (y + yc) / 2;
+                break;
+            default:
+                Console.WriteLine("Свитчер вышел за пределы значений");
+                break;
         }
     }
 }
